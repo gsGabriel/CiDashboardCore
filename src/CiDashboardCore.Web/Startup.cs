1f9eabf3b6b091622349b66e66887588f3ebdf6a
gsGabriel <gs.gabrielcfa@outlook.com>
2017-06-21 21:01:13 -0300

    add AspNet JavascriptServices and insert basic configuration for react spa apps

--- src/CiDashboardCore.Web/Startup.cs
+++ src/CiDashboardCore.Web/Startup.cs
@@ -42,6 +42,13 @@ namespace CiDashboardCore.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                //Since UseWebpackDevMiddleware needs to know which incoming requests to intercept, 
+                //make sure you've specified a publicPath value on your output
+                //app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                //{
+                //    HotModuleReplacement = true,
+                //    ReactHotModuleReplacement = true
+                //});
             }
             else
             {
@@ -55,8 +62,17 @@ namespace CiDashboardCore.Web
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                //Since the default server- side MVC route goes next, any requests that match existing controller / action pairs will be handled by invoking that action.
+                //Then, since MapSpaFallbackRoute is last, any other requests that don't appear to be for static files will be served by invoking the Index action on HomeController. 
+                //This action's view should serve your client-side application code, allowing the client-side routing system to handle whatever URL has been requested.
+                //routes.MapSpaFallbackRoute(
+                //    name: "spa-fallback",
+                //    defaults: new { controller = "Home", action = "Index" });
+
+                //for more information
+                //https://github.com/aspnet/JavaScriptServices/tree/dev/src/Microsoft.AspNetCore.SpaServices
             });
         }
-
     }
 }
